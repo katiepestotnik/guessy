04037f514c72134d5354e8ee4e7f1eac19efa274
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-09-14 20:03:30 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const game = {
+    title: 'Guess the number',
+    biggestNum: 10,
+    smallestNum: 1,
+    secretNum: null,
+    prevGuesses: [],
+    play: function (guess) {
+        this.secretNum = Math.floor(Math.random() * (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+        this.getGuess()
+    },
+    getGuess: function () {
+        let guess
+        while (guess !== game.secretNum) {
+            guess = parseInt(prompt(`Guess a number between ${this.smallestNum} and ${this.biggestNum} your previous guesses: * ${this.prevGuesses} *`))
+            this.prevGuesses.push(guess)
+            if (guess < game.secretNum) {
+                alert(`${guess} is too low, try again`)
+            } else if (guess > game.secretNum) {
+                alert(`${guess} is too high, try again`)
+            } else if (guess === game.secretNum) {
+                alert(`${guess} is correct, you got it after ${this.prevGuesses.length} times.`)
+            } else {
+                break
+            }
+        }
+    },
+    render: function () {
+
+    }
+}
+
+game.play()
+
+
